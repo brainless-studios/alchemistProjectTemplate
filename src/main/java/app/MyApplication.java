b73d364fd03273adff9670dd42c7030fab86bca8
methusalah <ben.oeilnoir@gmail.com>
2016-02-25 18:11:56 +0100

    project creation

---
+++ src/main/java/app/MyApplication.java
@@ -0,0 +1,15 @@
+package app;
+
+import view.Alchemist;
+
+public class MyApplication extends Alchemist {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	protected void onIntialize() {
+		// your code here
+	}
+}
